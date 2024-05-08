a9758c370d01cd70d98fa4388fae83cd2d51517e
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2024-05-08 16:14:26 +0300

    Task 2

---
+++ Conditional branching/Task 2/script.js
@@ -0,0 +1,7 @@
+let answer = prompt("What's the official name of JS?")
+
+if (answer == "ECMAScript" || answer == "ECMASCRIPT") {
+    alert("Right!")
+} else {
+    alert ("You don't know ?, it's ECMAScript!");
+}
