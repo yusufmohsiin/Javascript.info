6804597ea58524d01402c31b870e519ce1f5c3dc
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2024-05-08 15:54:49 +0300

    Task 1

---
+++ Conditional branching/Task 1/script.js
@@ -0,0 +1,11 @@
+if ("0") {
+    alert( 'Hello' );
+}
+/*
+=> Will alert be shown?
+- Yes, the value "0" in  if condition statement is a string, so it's a truthy value
+    truthy value remains true, so the equivalent code for this code will look like the following code
+        if (true) {
+            alert('Hello');
+        }
+*/
