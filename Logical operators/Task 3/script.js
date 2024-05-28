2237caff453c2f0ff2904e03630a9a86f1ee7a76
Yusuf Mohsen <youssef.mohsen277@gmail.com>
2024-05-28 20:42:30 +0300

    Task 3 v3

--- Logical operators/Task 3/script.js
+++ Logical operators/Task 3/script.js
@@ -1,11 +1,21 @@
 let userName = prompt("What is your username ?", '');
 
-if (userName == "Admin") {
-    var password = prompt("What is your password ?", "")
+if (userName === '' || userName === null) {
+    alert("Canceled")
+} else {
+    alert("I don't know you!")
 }
 
-if (password == "TheMaster") {
-    alert("Welcome!")
-} else {
-    password = prompt("Wrong password, please try again")
+
+
+if (userName == "Admin") {
+    let password = prompt("What is your password ?", "")
+
+    if (password === "TheMaster") {
+        alert("Welcome!");
+    } else if (password === '' || password === null) {
+        alert("Canceled")
+    } else {
+        alert("Wrong Password!")
+    }    
 }
